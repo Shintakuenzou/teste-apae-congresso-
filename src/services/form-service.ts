99f6ab387f6a520148672eebdb3ce24dbb8b7781
felipe enzou <felipe.enzou@gmail.com>
2026-01-30 16:01:49 -0300

    teste 1

--- src/services/form-service.ts
+++ src/services/form-service.ts
@@ -11,8 +11,9 @@ interface SendParticipantData {
 export async function handlePostFormParticipant({ documentId, values }: SendParticipantData) {
   console.log("values: ", values);
   console.log("values: ", import.meta.env.VITE_CONSUMER_KEY_BASE_TESTE);
+  console.log("documentId", documentId);
 
-  const response = await axiosApi.post(`/ecm-forms/api/v2/cardindex/${documentId}/cards`, { values });
+  const response = await axiosApi.post(`https://firebrick-kingfisher-525619.hostingersite.com/proxy.php`, { values });
   const data = response.data();
 
   return data;
