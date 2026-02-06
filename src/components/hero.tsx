adcc3a91a03488ad2f1b43039d6cf5dfbd483485
felipe enzou <felipe.enzou@gmail.com>
2026-02-06 10:18:36 -0300

    feat: adicionei a funcionalidade de trocar os elementos de texto em tela de acordo com os dados que foi alterado no fluig e essas alterações são refletida na aplicação via API em tempo real. além de arrumar os dados dos palestrantes fazendo ele vincular com o id do evento para que somente seja exibido aqueles palestrantes daquele evento.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -12,6 +12,8 @@ export function Hero() {
   let description;
   let date;
   let location;
+  console.log(formatedDataEvento);
+
   if (formatedDataEvento && formatedDataEvento.length > 0) {
     const idx = formatedDataEvento.length - 1;
     const tituloRaw = formatedDataEvento[idx]?.fields?.titulo ?? "";
